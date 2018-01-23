1b40e1355e1a315bdbbc4cc56811e3dfbf8bdca2
ISAACPC\isaactij <isaactij@utexas.edu>
2018-01-23 15:50:25 -0600

    Inital push

---
+++ TijerinaIsaac-hw1/TijerinaIsaac-hw1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TijerinaIsaac-hw1
+//
+//  Created by Tijerina, Isaac G on 1/23/18.
+//  Copyright © 2018 Tijerina, Isaac G. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
