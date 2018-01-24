3628024193151729bb33cf994430779be9ae3d11
Tijerina, Isaac G <igt86@lib-pclem042.lib.utexas.edu>
2018-01-24 17:11:29 -0600

    Finished code and added comments

--- TijerinaIsaac-hw1/TijerinaIsaac-hw1/AutomobileClass.swift
+++ TijerinaIsaac-hw1/TijerinaIsaac-hw1/AutomobileClass.swift
@@ -8,12 +8,15 @@
 
 import Foundation
 
+//Automobile class that is used in main.swift
 class Automobile{
+    //Variables that saves information about the Automobile
     private var _make:String
     private var _model:String
     private var _numberOfDoors:Int
     private var _speed:Int
     
+    //Sets the inital values of the Automobile variables
     init(_make:String, _model:String, _numberOfDoors:Int, _speed:Int) {
         self._make = _make
         self._model = _model
@@ -21,38 +24,47 @@ class Automobile{
         self._speed = _speed
     }
     
+    //When called creates a Automobile object using the provided parameters
     class func create(_make:String, _model:String, _numberOfDoors:Int, _speed:Int) -> Automobile{
         return Automobile(_make: _make,  _model: _model,_numberOfDoors : _numberOfDoors,_speed : _speed)
     }
     
-    func Get_make() -> String{
+    //Returns the make of the Automobile object
+    func getMake() -> String{
         return _make
     }
     
-    func Set_make(_make:String){
+    //Changes the make of the Automobile object
+    func setMake(_make:String){
         self._make = _make
     }
     
-    func Get_model() -> String{
+    //Returns the model of the Automobile object
+    func getModel() -> String{
         return _model
     }
     
-    func Set_model(_model:String){
+    //Changes the model of the Automobile object
+    func setModel(_model:String){
         self._model = _model
     }
     
-    func Get_numberOfDoors() -> Int{
+    //Returns the numberOfDoors of the Automobile object
+    func getNumberOfDoors() -> Int{
         return _numberOfDoors
     }
     
-    func Set_numberOfDoors(_numberOfDoors:Int){
+    //Changes the numberOfDoors of the Automobile object
+    func setNumberOfDoors(_numberOfDoors:Int){
         self._numberOfDoors = _numberOfDoors
     }
     
-    func Get_speed() -> Int{
+    //Returns the speed of the Automobile object
+    func getSpeed() -> Int{
         return _speed
     }
     
+    //Adds the provided speed to the current speed if the resulting speed is between 0 and 150
     func increaseSpeed(speedChange:Int){
         let changedSpeed = _speed + speedChange
         if(0 <= changedSpeed && changedSpeed <= 150){
@@ -60,6 +72,7 @@ class Automobile{
         }
     }
     
+    //Subtracts the provided speed from the current speed if the resulting speed is between 0 and 150
     func decreaseSpeed(speedChange:Int){
         let changedSpeed = _speed - speedChange
         if(0 <= changedSpeed && changedSpeed <= 150){
@@ -67,12 +80,12 @@ class Automobile{
         }
     }
     
+    //Retruns a string that contains all of the information about the Automobile
     func description() -> String{
         var description = "Make: " + _make
         description += ", Model: " + _model
         description += ", NumDoors: " + String(_numberOfDoors)
         description += ", Speed: " + String(_speed)
         return description
-    }
-    
+    }    
 }
--- TijerinaIsaac-hw1/TijerinaIsaac-hw1/main.swift
+++ TijerinaIsaac-hw1/TijerinaIsaac-hw1/main.swift
@@ -9,36 +9,89 @@
 import Foundation
 
 func main(){
-    var firstCar:Automobile = Automobile.create(_make: "Maserati", _model: "GranTurismo", _numberOfDoors: 2, _speed: 67)
-    var secondCar:Automobile = Automobile.create(_make: "Honda", _model: "Accord", _numberOfDoors: 4, _speed: 128)
-    var thirdCar:Automobile = Automobile.create(_make: "Tesla", _model: "S 90", _numberOfDoors: 2, _speed: 35)
-    var randomNum:Int
-    for i in 0...9{
-        randomNum = Int(randomValueBetween(min: 0, max: 16))
-        firstCar.increaseSpeed(speedChange: randomNum)
-        randomNum = Int(randomValueBetween(min: 0, max: 16))
-        secondCar.increaseSpeed(speedChange: randomNum)
-        randomNum = Int(randomValueBetween(min: 0, max: 16))
-        thirdCar.increaseSpeed(speedChange: randomNum)
-    }
+    //Create three Automobile objects
+    let firstCar:Automobile = Automobile.create(_make: "Maserati", _model: "GranTurismo", _numberOfDoors: 2, _speed: 67)
+    let secondCar:Automobile = Automobile.create(_make: "Honda", _model: "Accord", _numberOfDoors: 4, _speed: 128)
+    let thirdCar:Automobile = Automobile.create(_make: "Tesla", _model: "S 90", _numberOfDoors: 2, _speed: 35)
+    //Call changeSpeeds() to change their speeds simulating a race
+    changeSpeeds(firstCar: firstCar, secondCar: secondCar, thirdCar: thirdCar)
+    //Print out the descriptions of all three Automobile objects
     print(firstCar.description())
     print(secondCar.description())
     print(thirdCar.description())
-    var firstCarSpeed:Int = firstCar.Get_speed()
-    var secondCarSpeed:Int = secondCar.Get_speed()
-    var thirdCarSpeed:Int = thirdCar.Get_speed()
-    var winningCarMake:String = ""
-    var winningCarModel:String = ""
-    if(firstCarSpeed > secondCarSpeed && firstCarSpeed > thirdCarSpeed){
-        winningCarMake = firstCar.Get_make()
-        winningCarModel = firstCar.Get_model()
-    }
-    print(winningCarMake + " " + winningCarModel + " won!!")
-    
+    //Call raceOutcome() to evaluate who won or if there was a tie and saves what is returned
+    let outcome:String = raceOutcome(firstCar: firstCar, secondCar: secondCar, thirdCar: thirdCar)
+    //Print final results
+    print(outcome)
 }
 
+//Returns a random Int between provided min and max
 func randomValueBetween(min:UInt32, max:UInt32) -> UInt32 {
-    var randomValue:UInt32 = min + arc4random_uniform(UInt32(max - min + 1))
+    let randomValue:UInt32 = min + arc4random_uniform(UInt32(max - min + 1))
     return randomValue
 }
 
+//Simulates race by manipulating the speed variable in each Automobile object
+func changeSpeeds(firstCar: Automobile, secondCar: Automobile, thirdCar:Automobile){
+    //Creates variable to hold random Int
+    var randomNum:Int
+    //Loops for 10 cycles
+    for _ in 0...9{
+        //Get random Int
+        randomNum = Int(randomValueBetween(min: 0, max: 16))
+        //Adds the randomNum to the speed of the firstCar
+        firstCar.increaseSpeed(speedChange: randomNum)
+        //Get random Int
+        randomNum = Int(randomValueBetween(min: 0, max: 16))
+        //Adds the randomNum to the speed of the secondCar
+        secondCar.increaseSpeed(speedChange: randomNum)
+        //Get random Int
+        randomNum = Int(randomValueBetween(min: 0, max: 16))
+        //Adds the randomNum to the speed of the thirdCar
+        thirdCar.increaseSpeed(speedChange: randomNum)
+    }
+}
+
+//Evaluates the final speeds of the Automobile objects and returns a string indicating the results
+func raceOutcome(firstCar:Automobile, secondCar:Automobile, thirdCar:Automobile) -> String{
+    //Get the speeds of all three Automobile objects
+    let firstCarSpeed:Int = firstCar.getSpeed()
+    let secondCarSpeed:Int = secondCar.getSpeed()
+    let thirdCarSpeed:Int = thirdCar.getSpeed()
+    //Variables to hold the make and model of the winning car
+    var winningCarMake:String = ""
+    var winningCarModel:String = ""
+    //Indicates if there is a tie
+    var tie:Bool = false
+    //Checks to see if there is a tie and if so changes tie to true
+    if((firstCarSpeed == secondCarSpeed) && (firstCarSpeed > thirdCarSpeed)){
+        tie = true
+    }else if((firstCarSpeed == thirdCarSpeed) && (firstCarSpeed > secondCarSpeed)){
+        tie = true
+    }else if((secondCarSpeed == thirdCarSpeed) && (secondCarSpeed > firstCarSpeed)){
+        tie = true
+    //Checks to see which car has the highest speed and saves the make and model of the winning car
+    }else if((firstCarSpeed > secondCarSpeed) && (firstCarSpeed > thirdCarSpeed)){
+        winningCarMake = firstCar.getMake()
+        winningCarModel = firstCar.getMake()
+    }else if((secondCarSpeed > firstCarSpeed) && (secondCarSpeed > thirdCarSpeed)){
+        winningCarMake = secondCar.getMake()
+        winningCarModel = secondCar.getMake()
+    }else if((thirdCarSpeed > firstCarSpeed) && (thirdCarSpeed >  secondCarSpeed)){
+        winningCarMake = thirdCar.getMake()
+        winningCarModel = thirdCar.getMake()
+    }
+    //String to be returned
+    let returnString:String
+    //Creates the string to be returned based on if there is a tie of which car won
+    if(tie){
+        returnString = "There was a tie!"
+    }else{
+        returnString = winningCarMake + " " + winningCarModel + " won!!"
+    }
+    //returns the final outcome
+    return returnString
+}
+
+//Program begins to run here//
+main()
